96b3d629186542bdf81e9ad6c5b16593c89574ec
kazooie2424 <kazooie24@hotmail.co.uk>
2013-07-21 21:28:00 +0100

    Updated CopiousDogs.java Fixed spawning rates and Added mod to menu mod tab

--- Copious Dogs/Copious Dogs/com/gitHub/copiousDogs/CopiousDogs.java
+++ Copious Dogs/Copious Dogs/com/gitHub/copiousDogs/CopiousDogs.java
@@ -39,168 +39,172 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = "CopiousDogs", name = "Copious Dogs", version = "0.0.0.0")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false, versionBounds = "[0.0.0.0]",
-		channels = {Reference.CHANNEL_NAME}, packetHandler = PacketHandler.class)
+channels = {Reference.CHANNEL_NAME}, packetHandler = PacketHandler.class)
 public class CopiousDogs
 {
-	//
-	//Creative Tabs
-	//
-	public static CreativeTabs tabCopiousDogs = new CreativeTabs("tabCopiousDogs")
-	{
+	@EventHandler
+	public void Load(FMLInitializationEvent event) {
+	}
+
+//
+//Creative Tabs
+//
+public static CreativeTabs tabCopiousDogs = new CreativeTabs("tabCopiousDogs")
+{
         public ItemStack getIconItemStack()
         {
                 return new ItemStack(dogBiscuit, 1, 0);
         }
-	};
-	//
-	//Items
-	//
-	public static Item dogBiscuit;
-	public static Item dogCollar;
-	public static Item dogLeash;
-	
-	//
-	//Materials
-	//
-	public static final Material plate = (new Material(MapColor.ironColor));
-	
-	//
-	//Blocks
-	//
-	public static Block dogDish;
-	
-	@Instance("CopiousDogs")
-	public static CopiousDogs instance;
-	
-	@SidedProxy(clientSide = "com.gitHub.copiousDogs.client.ClientProxy", 
-			serverSide = "com.gitHub.copiousDogs.CommonProxy")
-	public static CommonProxy proxy;
-	
-	public static int dogDishRendererID;
+};
+//
+//Items
+//
+public static Item dogBiscuit;
+public static Item dogCollar;
+public static Item dogLeash;
+
+//
+//Materials
+//
+public static final Material plate = (new Material(MapColor.ironColor));
+
+//
+//Blocks
+//
+public static Block dogDish;
+
+@Instance("CopiousDogs")
+public static CopiousDogs instance;
+
+@SidedProxy(clientSide = "com.gitHub.copiousDogs.client.ClientProxy",
+serverSide = "com.gitHub.copiousDogs.CommonProxy")
+public static CommonProxy proxy;
+
+public static int dogDishRendererID;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event) 
+    public void preInit(FMLPreInitializationEvent event)
     {
-    	
-    	//Loads the copiousdogs.cfg config file
-    	Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-    	
-    	config.load();
-    	
-    	//reads the config file for item ids
-    	Reference.DOG_BISCUIT_ID = config.getItem("Dog biscuit:", 17001).getInt();
-    	Reference.EGG_GOLDEN_RETRIEVER_ID = config.getItem("Egg Golden Retriever:", 17002).getInt();
-    	Reference.DOG_COLLAR_ID = config.getItem("Dog collar:", 17003).getInt();
-    	Reference.EGG_HUSKY_ID = config.getItem("Egg Husky:", 17004).getInt();
-    	Reference.DOG_LEASH_ID = config.getItem("Dog leash:", 17005).getInt();
-    	Reference.EGG_BERNESE_MOUNTAIN_ID = config.getItem("Egg Bernese Mountain:", 17006).getInt();
-    	Reference.EGG_CHIHUAHUA_ID = config.getItem("Egg Chihuahua:", 17007).getInt();
-    	Reference.EGG_FRENCH_BULLDOG_ID = config.getItem("Egg French Bulldog:", 17008).getInt();
-    	
-    	//reads the config file for block ids
-    	Reference.DOG_DISH_ID = config.getBlock("Dog dish:", 1701).getInt();
-    	
-    	config.save();
+    
+     //Loads the copiousdogs.cfg config file
+     Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+    
+     config.load();
+    
+     //reads the config file for item ids
+     Reference.DOG_BISCUIT_ID = config.getItem("Dog biscuit:", 17001).getInt();
+     Reference.EGG_GOLDEN_RETRIEVER_ID = config.getItem("Egg Golden Retriever:", 17002).getInt();
+     Reference.DOG_COLLAR_ID = config.getItem("Dog collar:", 17003).getInt();
+     Reference.EGG_HUSKY_ID = config.getItem("Egg Husky:", 17004).getInt();
+     Reference.DOG_LEASH_ID = config.getItem("Dog leash:", 17005).getInt();
+     Reference.EGG_BERNESE_MOUNTAIN_ID = config.getItem("Egg Bernese Mountain:", 17006).getInt();
+     Reference.EGG_CHIHUAHUA_ID = config.getItem("Egg Chihuahua:", 17007).getInt();
+     Reference.EGG_FRENCH_BULLDOG_ID = config.getItem("Egg French Bulldog:", 17008).getInt();
+    
+     //reads the config file for block ids
+     Reference.DOG_DISH_ID = config.getBlock("Dog dish:", 1701).getInt();
+    
+     config.save();
     }
    
     @SuppressWarnings("unchecked")
-	@EventHandler
-    public void load(FMLInitializationEvent event) 
+@EventHandler
+    public void load(FMLInitializationEvent event)
     {
-    	proxy.registerRenderers();
-    	//
-    	//tab Copious dogs
-    	//
-    	LanguageRegistry.instance().addStringLocalization("itemGroup.tabCopiousDogs", "en_US", "Copious Dogs");
-    	//
-    	//Dog Biscuit
-    	//
-    	dogBiscuit = new DogBiscuit(Reference.DOG_BISCUIT_ID);
-    	LanguageRegistry.addName(dogBiscuit, "Dog Biscuit");
-    	//
-    	//Dog Collar
-    	//
-    	dogCollar = new DogCollar(Reference.DOG_COLLAR_ID);
-    	LanguageRegistry.addName(dogCollar, "Dog Collar");
-    	//
-    	//Dog leash
-    	//
-    	dogLeash = new DogLeash(Reference.DOG_LEASH_ID);
-    	LanguageRegistry.addName(dogLeash, "Dog Leash");
-    	//
-    	//Golden Retriever
-    	//
-    	EntityRegistry.registerModEntity(GoldenRetriever.class, "Golden Retriever", Reference.MOB_GOLDEN_RETRIEVER_ID, this, 40, 1, true);
-    	EntityRegistry.addSpawn(GoldenRetriever.class, 2, 2, 6, EnumCreatureType.creature, BiomeGenBase.plains);
-    	LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.Golden Retriever.name", "Golden Retriever");
-    	EntityList.IDtoClassMapping.put(Reference.EGG_GOLDEN_RETRIEVER_ID, GoldenRetriever.class);
-    	EntityList.entityEggs.put(Reference.EGG_GOLDEN_RETRIEVER_ID,
-    			new EntityEggInfo(Reference.EGG_GOLDEN_RETRIEVER_ID, 0xbc8e5f , 0xddcdb6));
-    	//
-    	//Husky
-    	//
-    	EntityRegistry.registerModEntity(Husky.class, "Husky", Reference.MOB_HUSKY_ID, this, 40, 1, true);
-    	EntityRegistry.addSpawn(Husky.class, 1, 2, 8, EnumCreatureType.creature, BiomeGenBase.icePlains, BiomeGenBase.taiga,
-    			BiomeGenBase.taigaHills, BiomeGenBase.iceMountains);
-    	LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.Husky.name", "Husky");
-    	EntityList.IDtoClassMapping.put(Reference.EGG_HUSKY_ID, Husky.class);
-    	EntityList.entityEggs.put(Reference.EGG_HUSKY_ID,
-    			new EntityEggInfo(Reference.EGG_HUSKY_ID, 0x2b2e2d , 0x7e807d));
-    	//
-    	//Bernese Mountain
-    	//
-    	EntityRegistry.registerModEntity(BerneseMountain.class, "Bernese Mountain", Reference.MOB_BERNESE_MOUNTAIN_ID, this, 40, 1, true);
-    	EntityRegistry.addSpawn(BerneseMountain.class, 1, 2, 8, EnumCreatureType.creature, BiomeGenBase.extremeHills, BiomeGenBase.extremeHillsEdge);
-    	LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.Bernese Mountain.name", "Bernese Mountain");
-    	EntityList.IDtoClassMapping.put(Reference.EGG_BERNESE_MOUNTAIN_ID, BerneseMountain.class);
-    	EntityList.entityEggs.put(Reference.EGG_BERNESE_MOUNTAIN_ID,
-    			new EntityEggInfo(Reference.EGG_BERNESE_MOUNTAIN_ID, 0x04070e , 0x723f21));
-    	//
-    	//Chihuahua
-    	//
-    	EntityRegistry.registerModEntity(Chihuahua.class, "Chihuahua", Reference.MOB_CHIHUAHUA_ID, this, 40, 1, true);
-    	EntityRegistry.addSpawn(Chihuahua.class, 1, 2, 8, EnumCreatureType.creature, BiomeGenBase.plains);
-    	LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.Chihuahua.name", "Chihuahua");
-    	EntityList.IDtoClassMapping.put(Reference.EGG_CHIHUAHUA_ID, Chihuahua.class);
-    	EntityList.entityEggs.put(Reference.EGG_CHIHUAHUA_ID,
-    			new EntityEggInfo(Reference.EGG_CHIHUAHUA_ID, 0xc7a087 , 0x9e7f6b));
-    	//
-    	//French Bulldog
-    	//
-    	EntityRegistry.registerModEntity(FrenchBullDog.class, "French Bulldog", Reference.MOB_FRENCH_BULLDOG_ID, this, 40, 1, true);
-    	EntityRegistry.addSpawn(FrenchBullDog.class, 1, 2, 8, EnumCreatureType.creature, BiomeGenBase.plains);
-    	LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.French Bulldog.name", "French Bulldog");
-    	EntityList.IDtoClassMapping.put(Reference.EGG_FRENCH_BULLDOG_ID, FrenchBullDog.class);
-    	EntityList.entityEggs.put(Reference.EGG_FRENCH_BULLDOG_ID,
-    			new EntityEggInfo(Reference.EGG_FRENCH_BULLDOG_ID, 0x151618 , 0xbdbdb7));
-    	//
-    	// Dog Dish
-    	//
-    	dogDish = new BlockDogDish(Reference.DOG_DISH_ID);
-    	GameRegistry.registerBlock(dogDish, DogDishItem.class, "dogDish");
-    	GameRegistry.registerTileEntity(TileEntityDogDish.class, "dog_dish_entity");
-    	LanguageRegistry.addName(dogDish, "Dog Dish");
-    	
-    	registerRecipes();
-    	proxy.registerRenderers();
+     proxy.registerRenderers();
+     //
+     //tab Copious dogs
+     //
+     LanguageRegistry.instance().addStringLocalization("itemGroup.tabCopiousDogs", "en_US", "Copious Dogs");
+     //
+     //Dog Biscuit
+     //
+     dogBiscuit = new DogBiscuit(Reference.DOG_BISCUIT_ID);
+     LanguageRegistry.addName(dogBiscuit, "Dog Biscuit");
+     //
+     //Dog Collar
+     //
+     dogCollar = new DogCollar(Reference.DOG_COLLAR_ID);
+     LanguageRegistry.addName(dogCollar, "Dog Collar");
+     //
+     //Dog leash
+     //
+     dogLeash = new DogLeash(Reference.DOG_LEASH_ID);
+     LanguageRegistry.addName(dogLeash, "Dog Leash");
+     //
+     //Golden Retriever
+     //
+     EntityRegistry.registerModEntity(GoldenRetriever.class, "Golden Retriever", Reference.MOB_GOLDEN_RETRIEVER_ID, this, 40, 1, true);
+     EntityRegistry.addSpawn(GoldenRetriever.class, 30, 2, 6, EnumCreatureType.creature, BiomeGenBase.plains);
+     LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.Golden Retriever.name", "Golden Retriever");
+     EntityList.IDtoClassMapping.put(Reference.EGG_GOLDEN_RETRIEVER_ID, GoldenRetriever.class);
+     EntityList.entityEggs.put(Reference.EGG_GOLDEN_RETRIEVER_ID,
+     new EntityEggInfo(Reference.EGG_GOLDEN_RETRIEVER_ID, 0xbc8e5f , 0xddcdb6));
+     //
+     //Husky
+     //
+     EntityRegistry.registerModEntity(Husky.class, "Husky", Reference.MOB_HUSKY_ID, this, 40, 1, true);
+     EntityRegistry.addSpawn(Husky.class, 30, 2, 8, EnumCreatureType.creature, BiomeGenBase.icePlains, BiomeGenBase.taiga,
+     BiomeGenBase.taigaHills, BiomeGenBase.iceMountains);
+     LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.Husky.name", "Husky");
+     EntityList.IDtoClassMapping.put(Reference.EGG_HUSKY_ID, Husky.class);
+     EntityList.entityEggs.put(Reference.EGG_HUSKY_ID,
+     new EntityEggInfo(Reference.EGG_HUSKY_ID, 0x2b2e2d , 0x7e807d));
+     //
+     //Bernese Mountain
+     //
+     EntityRegistry.registerModEntity(BerneseMountain.class, "Bernese Mountain", Reference.MOB_BERNESE_MOUNTAIN_ID, this, 40, 1, true);
+     EntityRegistry.addSpawn(BerneseMountain.class, 30, 2, 8, EnumCreatureType.creature, BiomeGenBase.extremeHills, BiomeGenBase.extremeHillsEdge);
+     LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.Bernese Mountain.name", "Bernese Mountain");
+     EntityList.IDtoClassMapping.put(Reference.EGG_BERNESE_MOUNTAIN_ID, BerneseMountain.class);
+     EntityList.entityEggs.put(Reference.EGG_BERNESE_MOUNTAIN_ID,
+     new EntityEggInfo(Reference.EGG_BERNESE_MOUNTAIN_ID, 0x04070e , 0x723f21));
+     //
+     //Chihuahua
+     //
+     EntityRegistry.registerModEntity(Chihuahua.class, "Chihuahua", Reference.MOB_CHIHUAHUA_ID, this, 40, 1, true);
+     EntityRegistry.addSpawn(Chihuahua.class, 30, 2, 8, EnumCreatureType.creature, BiomeGenBase.plains);
+     LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.Chihuahua.name", "Chihuahua");
+     EntityList.IDtoClassMapping.put(Reference.EGG_CHIHUAHUA_ID, Chihuahua.class);
+     EntityList.entityEggs.put(Reference.EGG_CHIHUAHUA_ID,
+     new EntityEggInfo(Reference.EGG_CHIHUAHUA_ID, 0xc7a087 , 0x9e7f6b));
+     //
+     //French Bulldog
+     //
+     EntityRegistry.registerModEntity(FrenchBullDog.class, "French Bulldog", Reference.MOB_FRENCH_BULLDOG_ID, this, 40, 1, true);
+     EntityRegistry.addSpawn(FrenchBullDog.class, 30, 2, 8, EnumCreatureType.creature, BiomeGenBase.plains);
+     LanguageRegistry.instance().addStringLocalization("entity.CopiousDogs.French Bulldog.name", "French Bulldog");
+     EntityList.IDtoClassMapping.put(Reference.EGG_FRENCH_BULLDOG_ID, FrenchBullDog.class);
+     EntityList.entityEggs.put(Reference.EGG_FRENCH_BULLDOG_ID,
+     new EntityEggInfo(Reference.EGG_FRENCH_BULLDOG_ID, 0x151618 , 0xbdbdb7));
+     //
+     // Dog Dish
+     //
+     dogDish = new BlockDogDish(Reference.DOG_DISH_ID);
+     GameRegistry.registerBlock(dogDish, DogDishItem.class, "dogDish");
+     GameRegistry.registerTileEntity(TileEntityDogDish.class, "dog_dish_entity");
+     LanguageRegistry.addName(dogDish, "Dog Dish");
+    
+     registerRecipes();
+     proxy.registerRenderers();
     }
    
     public void registerRecipes() {
-    	
-    	GameRegistry.addRecipe(new ItemStack(dogBiscuit), " m ", "mbm", " m ", 'm', Item.porkRaw, 'b', Item.bone);
-    	GameRegistry.addRecipe(new ItemStack(dogDish), "III", "IBI", "III", 'I', Item.ingotIron, 'B', Item.bucketEmpty);
-    	GameRegistry.addRecipe(new ItemStack(dogCollar), "SSS", "S S", "SSI", 'S', Item.silk, 'I', Item.ingotIron);
-    	GameRegistry.addRecipe(new ItemStack(dogLeash), "SS", "SS", "SS", 'S', Item.silk);
     
-    	for (int i = 0; i < 16; i++) {
-    		
-    		GameRegistry.addShapelessRecipe(new ItemStack(dogDish, 1, i), new ItemStack(Item.dyePowder, 1, BlockDogDish.getDyeFromBlock(i)), dogDish);
-    		GameRegistry.addShapelessRecipe(new ItemStack(dogCollar, 1, i), new ItemStack(Item.dyePowder, 1, DogCollar.getDyeFromItem(i)), dogCollar);
-    	}
+     GameRegistry.addRecipe(new ItemStack(dogBiscuit), " m ", "mbm", " m ", 'm', Item.porkRaw, 'b', Item.bone);
+     GameRegistry.addRecipe(new ItemStack(dogDish), "III", "IBI", "III", 'I', Item.ingotIron, 'B', Item.bucketEmpty);
+     GameRegistry.addRecipe(new ItemStack(dogCollar), "SSS", "S S", "SSI", 'S', Item.silk, 'I', Item.ingotIron);
+     GameRegistry.addRecipe(new ItemStack(dogLeash), "SS", "SS", "SS", 'S', Item.silk);
+    
+     for (int i = 0; i < 16; i++) {
+    
+     GameRegistry.addShapelessRecipe(new ItemStack(dogDish, 1, i), new ItemStack(Item.dyePowder, 1, BlockDogDish.getDyeFromBlock(i)), dogDish);
+     GameRegistry.addShapelessRecipe(new ItemStack(dogCollar, 1, i), new ItemStack(Item.dyePowder, 1, DogCollar.getDyeFromItem(i)), dogCollar);
+     }
     }
     
     @EventHandler
-    public void postInit(FMLPostInitializationEvent event) 
+    public void postInit(FMLPostInitializationEvent event)
     {
             // Stub Method
     }
