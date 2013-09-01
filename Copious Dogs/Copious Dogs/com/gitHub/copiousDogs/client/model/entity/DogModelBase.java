cbc4dacfc7199ad98be94ec541c67be6f2684f99
Dramentiaras <dramentiaras@server.fake>
2013-09-01 09:13:11 +0200

    Changed package tree to make more sense

---
+++ Copious Dogs/Copious Dogs/com/gitHub/copiousDogs/client/model/entity/DogModelBase.java
@@ -0,0 +1,67 @@
+package com.gitHub.copiousDogs.client.model.entity;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.MathHelper;
+
+import com.gitHub.copiousDogs.entity.Dog;
+
+public abstract class DogModelBase extends ModelBase
+{
+	ModelRenderer WolfHead;
+	ModelRenderer Body;
+	ModelRenderer Mane;
+	ModelRenderer Leg1;
+	ModelRenderer Leg2;
+	ModelRenderer Leg3;
+	ModelRenderer Leg4;
+	ModelRenderer Tail;
+	ModelRenderer Ear1;
+	ModelRenderer Ear2;
+	ModelRenderer Nose;
+
+	protected void setRotation(ModelRenderer model, float x, float y, float z)
+	{
+		model.rotateAngleX = x;
+		model.rotateAngleY = y;
+		model.rotateAngleZ = z;
+	}
+
+	public void setLivingAnimations(Dog entity, float walkTime,
+			float isWalking, float random)
+	{
+		if (entity.isTailAnimated())
+		{
+			this.Tail.rotateAngleY = MathHelper.cos(entity.ticksExisted * 0.8F) * 0.6F;
+		}
+		else
+		{
+			this.Tail.rotateAngleY = 0;
+		}
+		
+		this.Leg1.rotateAngleX = MathHelper.cos(walkTime * 0.6662F) * 1.4F * isWalking;
+		this.Leg2.rotateAngleX = MathHelper.cos(walkTime * 0.6662F + (float) Math.PI) * 1.4F * isWalking;
+		this.Leg3.rotateAngleX = MathHelper.cos(walkTime * 0.6662F + (float) Math.PI) * 1.4F * isWalking;
+		this.Leg4.rotateAngleX = MathHelper.cos(walkTime * 0.6662F) * 1.4F * isWalking;
+		
+		super.setLivingAnimations(entity, walkTime, isWalking, random);
+	}
+	
+	@Override
+	public void setLivingAnimations(EntityLivingBase entity, float walkTime,
+			float isWalking, float random)
+	{
+		setLivingAnimations((Dog)entity, walkTime, isWalking, random);
+	}
+
+	@Override
+	public void setRotationAngles(float f, float f1, float f2, float f3,
+			float f4, float f5, Entity entity)
+	{
+		//this.WolfHead.rotateAngleX = f4 / (180F / (float)Math.PI);
+        //this.WolfHead.rotateAngleY = f3 / (180F / (float)Math.PI);
+		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+	}
+}
